d27fe5ef20f260a255fc63d167bd0b1b8ed7ca30
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-26 18:58:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,20 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  row = 0 
+  join = []
+  while row < src.length do 
+    index = 0 
+    while index < src[row].length do 
+      if src[row][index].class == String
+        join.push(src[row][index])
+      end 
+      index += 1 
+    end 
+    row += 1 
+  end 
+  
+  
 end
 
-colin = "Colin" 
-puts colin.class
+
